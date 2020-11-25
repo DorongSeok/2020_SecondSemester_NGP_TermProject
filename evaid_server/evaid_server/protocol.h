a7d78f5db0290ff204b7bc0eb1dc572187fc9160
Kim Young June <rladudwns4643@gmail.com>
2020-11-25 13:04:44 +0900

    multi thread echo server

--- evaid_server/evaid_server/protocol.h
+++ evaid_server/evaid_server/protocol.h
@@ -1,34 +1,38 @@
 #pragma once
+
+#define SERVERIP "127.0.0.1"
+#define SERVERPORT 9000
 #define MAXBUFFER 1024
 
-constexpr int CS_1 = 1;
-constexpr int CS_2 = 2;
-constexpr int CS_3 = 3;
-constexpr int CS_4 = 4;
+constexpr BYTE CS_1 = 1;
+constexpr BYTE CS_2 = 2;
+constexpr BYTE CS_3 = 3;
+constexpr BYTE CS_4 = 4;
 
 #pragma pack(1)
 class cs_packet_1 {
 public:
-	short size;
-	char type;
-	char data;
+	BYTE size;	//1
+	BYTE type;	//1
+	BYTE data;	//1
 };
 class cs_packet_2 {
 public:
-	short size;
-	char type;
-	char data;
+	BYTE size;
+	BYTE type;
+	BYTE data;
 };
 class cs_packet_3 {
 public:
-	short size;
-	char type;
-	char data;
+	BYTE size;
+	BYTE type;
+	BYTE data_1;
+	BYTE data_2;
 };
 class cs_packet_4 {
 public:
-	short size;
-	char type;
-	char data;
+	BYTE size;
+	BYTE type;
+	BYTE data;
 };
 #pragma pack(pop)
